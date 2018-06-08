ec982b2c49474e53360f7a3f130f0f175b0d357e
Sheepsaurus <6517410+Sheepsaurus@users.noreply.github.com>
2018-06-08 12:53:23 +0200

    OptionsMenuNames

--- Guideviewer/Options.xaml.cs
+++ Guideviewer/Options.xaml.cs
@@ -22,6 +22,24 @@ namespace Guideviewer
         public Options()
         {
             InitializeComponent();
+
+
+        }
+
+        private void CheckboxHandler(object sender, RoutedEventArgs routedEventArgs) {
+            if (Db.IsChecked == true) {
+                D1.IsChecked = true;
+                D2.IsChecked = true;
+                D3.IsChecked = true;
+                D4.IsChecked = true;
+                D5.IsChecked = true;
+                D6.IsChecked = true;
+                D7.IsChecked = true;
+                D8.IsChecked = true;
+                B1.IsChecked = true;
+                B2.IsChecked = true;
+                B3.IsChecked = true;
+            }
         }
     }
 }
